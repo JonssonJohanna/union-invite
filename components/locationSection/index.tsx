5fabaf1381ecfbde020584d4724bc53c659b34e6
JonssonJohannae <johanna_jonson@hotmail.com>
2024-08-19 19:56:13 +0200

    Add google location.

--- components/locationSection/index.tsx
+++ components/locationSection/index.tsx
@@ -9,6 +9,12 @@ import Button from '../Button';
 import Icon from '../Icon';
 
 const LocationSection: React.FC = () => {
+  const locationOnClick = () => {
+    const location =
+      'Updown Farmhouse, Updown Rd, Betteshanger, Deal CT14 0EF, Storbritannien';
+    const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${location}`;
+    window.open(googleMapsUrl, '_blank');
+  };
   return (
     <>
       <LocationContainer>
@@ -17,7 +23,9 @@ const LocationSection: React.FC = () => {
           <LocationText>
             Updown Farm, Updown Road, Betteshanger, Deal, CT14 0EF
           </LocationText>
-          <Button type='outlined'>Directions</Button>
+          <Button type='outlined' onClick={locationOnClick}>
+            Directions
+          </Button>
         </TextWrapper>
         <Icon imageSrc='updown.png' size={500}></Icon>
       </LocationContainer>
